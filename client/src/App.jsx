df6cd177858d956df8534cd12d8e879b1ad3406a
Gui <gsardaparreu@gmail.com>
2023-02-23 16:14:54 +0100

    style: apply airbnb eslint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-
-  return (
-    <div className="App">
-     
-    </div>
-  )
+  return <div className="App" />;
 }
 
-export default App
+export default App;
