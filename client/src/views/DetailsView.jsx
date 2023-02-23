c45b88e2df3b9a5d97d0d2d9ae0200a3402f9aa4
Gui <gsardaparreu@gmail.com>
2023-02-23 16:32:43 +0100

    feat: add views

---
+++ client/src/views/DetailsView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './DetailsView.css';
+
+function DetailsView() {
+  return <main />;
+}
+
+export default DetailsView;
---
+++ client/src/views/ListView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ListView.css';
+
+function ListView() {
+  return <main />;
+}
+
+export default ListView;
