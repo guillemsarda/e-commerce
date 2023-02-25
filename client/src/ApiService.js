8c7d54ab203d41c74ea261a75851204792078eb1
Gui <gsardaparreu@gmail.com>
2023-02-25 14:17:59 +0100

    feat: add details boilerplate

--- client/src/ApiService.js
+++ client/src/ApiService.js
@@ -1,24 +1,26 @@
 const baseUrl = 'https://itx-frontend-test.onrender.com/api/';
 
-async function getProducts(setter, errorSetter, setLoading) {
+async function getProducts(setter, setError, setLoading) {
   try {
     const res = await fetch(`${baseUrl}product`);
+    if (res.status > 400) throw new Error('Could not get products :(');
     const productList = await res.json();
     setter(productList);
   } catch (error) {
-    errorSetter('Could not get products :(');
+    setError(error.message);
   } finally {
     setLoading(false);
   }
 }
 
-async function getProductById(id, setter, errorSetter, setLoading) {
+async function getProductById(id, setter, setError, setLoading) {
   try {
-    const res = await fetch(`${baseUrl}product/id`);
+    const res = await fetch(`${baseUrl}product/${id}`);
+    if (res.status > 400) throw new Error('Product not found.');
     const productDetail = await res.json();
     setter(productDetail);
   } catch (error) {
-    errorSetter('Product not found.');
+    setError(error.message);
   } finally {
     setLoading(false);
   }
--- client/src/views/DetailsView.jsx
+++ client/src/views/DetailsView.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './DetailsView.css';
 
+import { Player } from '@lottiefiles/react-lottie-player';
+import { getProductById } from '../ApiService';
+import Fallback from '../components/Fallback';
+
 function DetailsView() {
-  return <main>Details View</main>;
+  const { id } = useParams();
+  const [details, setDetails] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getProductById(id, setDetails, setError, setLoading);
+  }, [id]);
+  if (loading) {
+    return (
+      <Player
+        autoplay
+        loop
+        src="https://assets3.lottiefiles.com/packages/lf20_0xt1vcey.json"
+        style={{ height: '300px', width: '300px' }}
+      />
+    );
+  }
+
+  if (error) {
+    return <Fallback text={error} />;
+  }
+
+  return <main>{details.brand}</main>;
 }
 
 export default DetailsView;
